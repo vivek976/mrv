1d8855240ff68e7950ec348dd9ee45899d6a70d3
vivek976 <72372388+vivek976@users.noreply.github.com>
2023-11-22 19:30:02 +0530

    storageDetails.js

--- controllers/storageDetails.js
+++ controllers/storageDetails.js
@@ -1,5 +1,7 @@
 const express=require("express")
 const router=express.Router()
+const xlsx=require("xlsx")
+const moment=require("moment")
 const dbConnect=require("../db/connect")
 dbConnect(process.env.url)
 const storage=require("../models/storage.schema")
@@ -9,6 +11,18 @@ router.post("/storageDetails",async(req,res)=>
     {
     const newStorage=new storage(req.body)
     const Storage=await storage.create(newStorage)
+    const data=await storage.find().lean().exec()
+    const worksheetData = data.map(item => ({
+        ...item,
+        dateOfOperation: moment(item.dateOfOperation).format('YYYY-MM-DD'),
+        labour: JSON.stringify(item.labour)   
+    }));
+    
+    const worksheet=xlsx.utils.json_to_sheet(worksheetData)
+    const workbook=xlsx.utils.book_new()
+    xlsx.utils.book_append_sheet(workbook,worksheet,"sheet1");
+    const exportfilepath="storageDetails.xlsx"
+    xlsx.writeFile(workbook,exportfilepath)
     res.send(Storage)
     }
     catch(error)
