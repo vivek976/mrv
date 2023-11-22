ccde8741b370869e54d9b43f3cecc51a55a2151c
vivek976 <72372388+vivek976@users.noreply.github.com>
2023-11-22 19:27:50 +0530

    irrigationDetails.js

--- controllers/irrigationDetails.js
+++ controllers/irrigationDetails.js
@@ -1,5 +1,7 @@
-const express=require("express")
+/*const express=require("express")
 const router=express.Router()
+const xlsx = require('xlsx');
+const moment = require('moment');
 const dbConnect=require("../db/connect")
 dbConnect(process.env.url)
 const irrigation=require("../models/irrigation.schema")
@@ -15,6 +17,13 @@ router.post('/irrigationDetails', async (req, res) => {
         table_data: irrigationDetails, 
       });
       const savedIrrigation = await newIrrigationTable.save();
+      const data = await irrigation.find().lean().exec()
+      const worksheetData=data.map(item=>({...item,table_data:JSON.stringify(item.table_data)}))
+      const worksheet=xlsx.utils.json_to_sheet(worksheetData)
+      const workbook=xlsx.utils.book_new()
+      xlsx.utils.book_append_sheet(workbook,worksheet,"sheet1");
+      const exportfilepath="filename3.xlsx"
+      xlsx.writeFile(workbook,exportfilepath)
 
     res.status(201).json(savedIrrigation);
   } catch (error) {
@@ -22,3 +31,46 @@ router.post('/irrigationDetails', async (req, res) => {
   }
 });
 module.exports=router;
+*/
+
+
+
+const express = require('express');
+const router=express.Router()
+const mongoose = require('mongoose');
+const XLSX = require('xlsx');
+const moment = require('moment');
+const ExcelJS = require('exceljs');
+const dbConnect=require("../db/connect")
+dbConnect(process.env.url)
+const IrrigationModel=require("../models/irrigation.schema")
+router.post('/irrigationDetails', async (req, res) => {
+  try {
+    const irrigationData = await IrrigationModel.find().lean();
+
+    if (!irrigationData || irrigationData.length === 0) {
+      return res.status(404).json({ message: 'No data found' });
+    }
+    const modifiedData = irrigationData.map((data) => ({
+      ...data,
+      
+      table_data: JSON.stringify(data.table_data),
+    }));
+    const worksheet = XLSX.utils.json_to_sheet(modifiedData);
+    const workbook = XLSX.utils.book_new();
+    XLSX.utils.book_append_sheet(workbook, worksheet, 'Irrigation Data');
+    const exportfilepath="irrigationDetails.xlsx"
+    XLSX.writeFile(workbook,exportfilepath)
+    res.send()
+    
+
+  
+  } catch (error) {
+    console.error('Error:', error);
+    res.status(500).json({ message: 'Internal server error' });
+  }
+});
+
+
+module.exports=router;
+
